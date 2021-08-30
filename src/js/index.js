a8504d82c79d06009903d8ddff430b1894345e47
Claudiu <12200607+nclaudiuf@users.noreply.github.com>
2021-08-30 07:09:01 +0700

    added bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 // import Popper from 'popper.js';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {jarallax, jarallaxElement} from 'jarallax'; //Wiki & Credits: https://github.com/nk-o/jarallax
 import 'aos';
 //import imagesLoaded from 'imagesloaded';
